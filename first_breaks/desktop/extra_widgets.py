1eee0f0f67fee424959459d9dfc828db00ad4198
Aleksei Tarasov <aleksei.v.tarasov@gmail.com>
2023-03-07 17:02:00 +0100

    upd

---
+++ first_breaks/desktop/extra_widgets.py
@@ -0,0 +1,27 @@
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QLabel, QDialog, QDialogButtonBox, QDesktopWidget, QVBoxLayout
+
+
+class WarnBox(QDialog):
+
+    def __init__(self, parent, title='Error', message='Error'):
+        super().__init__(parent=parent)
+
+        self.setWindowTitle(title)
+        self.setGeometry(100, 100, 350, 100)
+
+        qt_rectangle = self.frameGeometry()
+        center_point = QDesktopWidget().availableGeometry().center()
+        qt_rectangle.moveCenter(center_point)
+        self.move(qt_rectangle.topLeft())
+
+        QBtn = QDialogButtonBox.Ok
+        self.buttonBox = QDialogButtonBox(QBtn)
+        self.buttonBox.accepted.connect(self.accept)
+        error_label = QLabel(message)
+        error_label.setTextInteractionFlags(Qt.LinksAccessibleByMouse|Qt.TextSelectableByMouse)
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(error_label)
+        self.layout.addWidget(self.buttonBox)
+        self.setLayout(self.layout)
