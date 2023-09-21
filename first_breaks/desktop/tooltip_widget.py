51dd156441e193da626626ae00698909bd68720f
DaloroAT <aleksei.v.tarasov@gmail.com>
2023-09-21 14:02:53 +0200

    tooltips

--- first_breaks/desktop/tooltip_widget.py
+++ first_breaks/desktop/tooltip_widget.py
@@ -1,247 +1,11 @@
 import sys
 from typing import Union, Sequence, Optional, Callable, Any, Tuple
+from weakref import WeakKeyDictionary
 
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLineEdit, QLabel, QFrame
 
 from PyQt5.QtGui import QPalette, QColor
 from PyQt5.QtCore import Qt, QTimer, QPoint, QEasingCurve, QPropertyAnimation, pyqtSignal, QObject, pyqtBoundSignal
-
-
-# class GeneralToolTip(QLabel):
-#     def __init__(self, widget, text=None, highlight=False, shake=False, duration=1000):
-#         super().__init__(None)  # No parent to be a top-level window
-#
-#         # ToolTip Settings
-#         self.setFrameShape(QFrame.StyledPanel)
-#         self.setStyleSheet("""
-#             background-color: yellow;
-#             border: 1px solid black;
-#             padding: 2px;
-#         """)
-#         self.setAutoFillBackground(True)
-#         self.setWindowFlags(Qt.ToolTip | Qt.WindowStaysOnTopHint)
-#
-#         # If there's a message, show it.
-#         if text:
-#             self.setText(text)
-#             global_pos = widget.mapToGlobal(QPoint(0, 0))
-#             self.move(global_pos.x() + widget.width(), global_pos.y())
-#             self.show()
-#
-#         # Highlighting the target widget
-#         if highlight:
-#             self.original_palette = widget.palette()
-#             palette = widget.palette()
-#             palette.setColor(QPalette.Base, QColor(255, 220, 220))  # Light red
-#             widget.setPalette(palette)
-#
-#             QTimer.singleShot(duration, lambda: widget.setPalette(self.original_palette))
-#
-#         # Shaking the target widget
-#         if shake:
-#             self.animation = QPropertyAnimation(widget, b"pos")
-#             original_pos = widget.pos()
-#             self.animation.setStartValue(original_pos)
-#             self.animation.setEndValue(original_pos)
-#             num_shakes = 20
-#             for i in range(num_shakes):
-#                 self.animation.setKeyValueAt((i + 1) / num_shakes, original_pos + (-1) ** i * QPoint(1, 0))
-#             self.animation.setDuration(duration)
-#
-#             self.animation.setEasingCurve(QEasingCurve.InOutQuad)
-#             self.animation.start()
-#
-#         QTimer.singleShot(duration, self.close)
-
-
-class QErrorToolTip(QLabel):
-    index_changed_signal = pyqtSignal()
-
-    def __init__(self,
-                 widget,
-                 text=None,
-                 text_position="top",
-                 highlight_widgets=None,
-                 shake_widgets=None,
-                 text_duration=1000,
-                 shake_duration=1000,
-                 highlight_duration=1000,
-                 show_until_event=None):
-
-        super().__init__(widget)
-        text_duration = max(1, int(text_duration))
-        shake_duration = max(1, int(shake_duration))
-        highlight_duration = max(1, int(highlight_duration))
-
-        self._original_palettes = {}
-        self._shake_animations = []
-
-        if text:
-            self.show_text(widget, text, text_position, text_duration, show_until_event)
-
-        if highlight_widgets:
-            for w in highlight_widgets:
-                self.highlight_widget(w, highlight_duration)
-
-        if shake_widgets:
-            for w in shake_widgets:
-                self.shake_widget(w, shake_duration)
-
-        if show_until_event:
-            if not isinstance(show_until_event, (list, tuple)):
-                show_until_event = [show_until_event]
-            for ev in show_until_event:
-                ev.connect(self.close)
-        else:
-            max_duration = int(1.2 * max(text_duration, shake_duration, highlight_duration))
-            QTimer.singleShot(max_duration, self.close)
-
-    def show_text(self, widget, text, text_position, text_duration, show_until_event):
-        self.setWordWrap(True)
-        self.setFrameShape(QFrame.StyledPanel)
-        self.setStyleSheet("""
-            background-color: yellow;
-            border: 1px solid black;
-            padding: 2px;
-        """)
-        self.setAutoFillBackground(True)
-        self.setWindowFlags(Qt.ToolTip | Qt.WindowStaysOnTopHint)
-
-        self.setText(text)
-        global_pos = widget.mapToGlobal(QPoint(0, 0))
-        if text_position == "top":
-            self.move(global_pos.x(), global_pos.y() - self.height())
-        elif text_position == "bottom":
-            self.move(global_pos.x(), global_pos.y() + widget.height() + 10)
-        elif text_position == "left":
-            self.move(global_pos.x() - self.width() - widget.width(), global_pos.y())
-        elif text_position == "right":
-            self.move(global_pos.x() + widget.width() + 10, global_pos.y())
-        else:
-            raise ValueError("Wrong position value")
-        self.show()
-
-        if show_until_event:
-            pass
-        else:
-            QTimer.singleShot(text_duration, self.hide)
-
-    def shake_widget(self, widget, shake_duration):
-        shakes_per_second = 20
-        animation = QPropertyAnimation(widget, b"pos")
-        self._shake_animations.append(animation)
-        original_pos = widget.pos()
-        animation.setStartValue(original_pos)
-        animation.setEndValue(original_pos)
-        num_shakes = max(1, int(shakes_per_second * shake_duration / 1000))
-        for i in range(num_shakes):
-            animation.setKeyValueAt((i + 1) / num_shakes, original_pos + (-1) ** i * QPoint(1, 0))
-        animation.setDuration(shake_duration)
-        animation.setEasingCurve(QEasingCurve.InOutQuad)
-        animation.start()
-
-    def highlight_widget(self, widget, highlight_duration):
-        self._original_palettes[widget] = widget.palette()
-        palette = widget.palette()
-        palette.setColor(QPalette.Base, QColor(255, 220, 220))  # Light red
-        widget.setPalette(palette)
-
-        QTimer.singleShot(highlight_duration, lambda: widget.setPalette(self._original_palettes[widget]))
-
-
-# class QErrorToolTip(QLabel):
-#     index_changed_signal = pyqtSignal()
-#
-#     def __init__(self,
-#                  widget: QWidget,
-#                  text: Optional[str] = None,
-#                  text_position: str = "top",
-#                  text_until: Union[float, pyqtSignal, Sequence[pyqtSignal]] = 1000,
-#                  highlight_widgets: Optional[Union[QWidget, Sequence[QWidget]]] = None,
-#                  highlight_until: Union[float, pyqtSignal, Sequence[pyqtSignal]] = 1000,
-#                  shake_widgets: Optional[Union[QWidget, Sequence[QWidget]]] = None,
-#                  shake_duration: Union[float, pyqtSignal, Sequence[pyqtSignal]] = 1000,
-#                  ):
-#
-#         super().__init__(widget)
-#         text_duration = max(1, int(text_duration))
-#         shake_duration = max(1, int(shake_duration))
-#         highlight_duration = max(1, int(highlight_duration))
-#
-#         self._original_palettes = {}
-#         self._shake_animations = []
-#
-#         if text:
-#             self.show_text(widget, text, text_position, text_duration, show_until_event)
-#
-#         if highlight_widgets:
-#             for w in highlight_widgets:
-#                 self.highlight_widget(w, highlight_duration)
-#
-#         if shake_widgets:
-#             for w in shake_widgets:
-#                 self.shake_widget(w, shake_duration)
-#
-#         if show_until_event:
-#             if not isinstance(show_until_event, (list, tuple)):
-#                 show_until_event = [show_until_event]
-#             for ev in show_until_event:
-#                 ev.connect(self.close)
-#         else:
-#             max_duration = int(1.2 * max(text_duration, shake_duration, highlight_duration))
-#             QTimer.singleShot(max_duration, self.close)
-#
-#     def show_text(self, widget, text, text_position, text_duration, show_until_event):
-#         self.setWordWrap(True)
-#         self.setFrameShape(QFrame.StyledPanel)
-#         self.setStyleSheet("""
-#             background-color: yellow;
-#             border: 1px solid black;
-#             padding: 2px;
-#         """)
-#         self.setAutoFillBackground(True)
-#         self.setWindowFlags(Qt.ToolTip | Qt.WindowStaysOnTopHint)
-#
-#         self.setText(text)
-#         global_pos = widget.mapToGlobal(QPoint(0, 0))
-#         if text_position == "top":
-#             self.move(global_pos.x(), global_pos.y() - self.height())
-#         elif text_position == "bottom":
-#             self.move(global_pos.x(), global_pos.y() + widget.height() + 10)
-#         elif text_position == "left":
-#             self.move(global_pos.x() - self.width() - widget.width(), global_pos.y())
-#         elif text_position == "right":
-#             self.move(global_pos.x() + widget.width() + 10, global_pos.y())
-#         else:
-#             raise ValueError("Wrong position value")
-#         self.show()
-#
-#         if show_until_event:
-#             pass
-#         else:
-#             QTimer.singleShot(text_duration, self.hide)
-#
-#     def shake_widget(self, widget, shake_duration):
-#         shakes_per_second = 20
-#         animation = QPropertyAnimation(widget, b"pos")
-#         self._shake_animations.append(animation)
-#         original_pos = widget.pos()
-#         animation.setStartValue(original_pos)
-#         animation.setEndValue(original_pos)
-#         num_shakes = max(1, int(shakes_per_second * shake_duration / 1000))
-#         for i in range(num_shakes):
-#             animation.setKeyValueAt((i + 1) / num_shakes, original_pos + (-1) ** i * QPoint(1, 0))
-#         animation.setDuration(shake_duration)
-#         animation.setEasingCurve(QEasingCurve.InOutQuad)
-#         animation.start()
-#
-#     def highlight_widget(self, widget, highlight_duration):
-#         self._original_palettes[widget] = widget.palette()
-#         palette = widget.palette()
-#         palette.setColor(QPalette.Base, QColor(255, 220, 220))  # Light red
-#         widget.setPalette(palette)
-#
-#         QTimer.singleShot(highlight_duration, lambda: widget.setPalette(self._original_palettes[widget]))
 
 
 TDoUntil = Union[int, float, pyqtSignal, Sequence[pyqtSignal]]
@@ -296,34 +60,112 @@ class TextToolTip(QLabel):
         run_do_until(do_until, self.close)
 
 
+class _HighlightManager:
+    _original_colors = WeakKeyDictionary()
+    _highlight_counts = WeakKeyDictionary()
+
+    @classmethod
+    def highlight_widget(cls, widget: QWidget, color: Tuple[int, int, int]):
+        if widget not in cls._original_colors:
+            cls._original_colors[widget] = widget.palette()
+
+        if widget not in cls._highlight_counts:
+            cls._highlight_counts[widget] = 0
+
+        cls._highlight_counts[widget] += 1
+        palette = widget.palette()
+        palette.setColor(QPalette.Base, QColor(*color))
+        widget.setPalette(palette)
+
+    @classmethod
+    def remove_highlight(cls, widget: QWidget):
+        if widget in cls._highlight_counts:
+            cls._highlight_counts[widget] -= 1
+            if cls._highlight_counts[widget] <= 0:
+                widget.setPalette(cls._original_colors[widget])
+                del cls._original_colors[widget]
+                del cls._highlight_counts[widget]
+
+
 class HighlightToolTip(QObject):
-    _all_highlight_tips = []
+    _manager = _HighlightManager()
 
-    def __init__(self,
-                 widgets: Optional[Union[QWidget, Sequence[QWidget]]] = None,
-                 do_until: TDoUntil = 5000):
-        super().__init__()
+    def __init__(
+            self,
+            widgets: Union[QWidget, Sequence[QWidget]],
+            parent: Optional[QWidget] = None,
+            do_until: TDoUntil = 5000,
+            color=(255, 220, 220),):
+        if not parent and widgets:
+            if isinstance(widgets, (list, tuple)):
+                parent = widgets[0]
+            else:
+                parent = widgets
+        super().__init__(parent)
 
-        widgets = widgets if isinstance(widgets, (list, tuple)) else [widgets]
-        self._original_palettes = {}
-        for widget in widgets:
-            self._original_palettes[widget] = widget.palette()
-            palette = widget.palette()
-            palette.setColor(QPalette.Base, QColor(255, 220, 220))  # Light red
-            widget.setPalette(palette)
-            # widget._highlight_tooltip_auto_attribute = self
+        self.widgets = widgets if isinstance(widgets, (list, tuple)) else [widgets]
 
-        HighlightToolTip._all_highlight_tips.append(self)
+        for widget in self.widgets:
+            self._manager.highlight_widget(widget, color)
 
-        run_do_until(do_until, self.remove_highlight)
+        run_do_until(do_until, self.remove_highlights)
 
-        print(HighlightToolTip._all_highlight_tips)
+    def remove_highlights(self) -> None:
+        for widget in self.widgets:
+            self._manager.remove_highlight(widget)
+        self.deleteLater()
 
-    def remove_highlight(self):
-        for widget, original_color in self._original_palettes.items():
-            widget.setPalette(self._original_palettes[widget])
 
-        HighlightToolTip._all_highlight_tips.remove(self)
+class _Shaker:
+    def __init__(self, widget: QWidget):
+        self._widget = widget
+        self._start_pos = widget.pos()
+        self._animation = QPropertyAnimation(widget, b"pos")
+        self._animation.setStartValue(self._start_pos)
+        self._animation.setEndValue(self._start_pos)
+
+        basic_duration = 1000
+        shakes_per_second = 10
+
+        num_shakes = max(1, int(shakes_per_second * basic_duration / 1000))
+        for i in range(num_shakes):
+            self._animation.setKeyValueAt((i + 1) / num_shakes, self._start_pos + (-1) ** i * QPoint(2, 0))
+
+        self._animation.setDuration(basic_duration)
+        self._animation.setLoopCount(-1)
+        self._animation.setEasingCurve(QEasingCurve.InOutQuad)
+
+    def shake(self):
+        self._animation.start()
+
+    def stop(self):
+        self._animation.stop()
+        self._widget.move(self._start_pos)
+
+
+class ShakeToolTip(QObject):
+    def __init__(self, widgets: Union[QWidget, Sequence[QWidget]], parent: Optional[QWidget] = None, do_until: TDoUntil = 5000):
+        if not parent and widgets:
+            if isinstance(widgets, (list, tuple)):
+                parent = widgets[0]
+            else:
+                parent = widgets
+        super().__init__(parent)
+
+        self._shakers = []
+        self.widgets = widgets if isinstance(widgets, (list, tuple)) else [widgets]
+
+        for widget in self.widgets:
+            shaker = _Shaker(widget)
+            shaker.shake()
+            self._shakers.append(shaker)
+
+        run_do_until(do_until, self.stop_shaking)
+
+    def stop_shaking(self):
+        for shaker in self._shakers:
+            shaker.stop()
+        self.deleteLater()
 
 
 if __name__ == "__main__":
@@ -339,19 +181,12 @@ if __name__ == "__main__":
             self.line_edit.textChanged.connect(self.validate_input)
 
         def validate_input(self, text):
-            # print(getattr(self.line_edit, "_highlight_tooltip_auto_attribute", None))
             if "@" not in text:
-                # QErrorToolTip(self.line_edit, text="Password must contain '@' symbol!",
-                #               text_position="bottom",
-                #               highlight_widgets=[self.line_edit],
-                #               shake_widgets=[self.line_edit],
-                #               text_duration=10,
-                #               shake_duration=1000,
-                #               highlight_duration=3000,
-                #               show_until_event=self.line_edit.textChanged
-                #               )
-                # TextToolTip(self.line_edit, text="Password must contain '@' symbol!" * 1, do_until=self.line_edit.textChanged)
+                TextToolTip(self.line_edit,
+                            text="Password must contain '@' symbol!" * 1,
+                            do_until=self.line_edit.textChanged)
                 HighlightToolTip(self.line_edit, do_until=self.line_edit.textChanged)
+                ShakeToolTip(self.line_edit, do_until=self.line_edit.textChanged)
 
 
     app = QApplication(sys.argv)
