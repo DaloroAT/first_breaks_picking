e2b56a9804546f84f75b7e1be39c451ce3c644d9
DaloroAT <aleksei.v.tarasov@gmail.com>
2023-09-21 15:11:24 +0200

    bandfilter

--- first_breaks/desktop/bandfilter_widget.py
+++ first_breaks/desktop/bandfilter_widget.py
@@ -1,14 +1,13 @@
 from typing import Any, Optional
 
-from PyQt5.QtCore import Qt, pyqtSignal
 from PyQt5.QtGui import QDoubleValidator
-from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QSlider, QWidget, QLineEdit
+from PyQt5.QtWidgets import QApplication, QHBoxLayout, QLabel, QWidget, QLineEdit, QCheckBox
 
-from first_breaks.desktop.tooltip_widget import QErrorToolTip
+from first_breaks.desktop.tooltip_widget import TextToolTip, ShakeToolTip, HighlightToolTip
 
 
 class QBandFilterWidget(QWidget):
-    def __init__(self, margins: Optional[int] = None, *args: Any, **kwargs: Any):
+    def __init__(self, margins: Optional[int] = None, debug: bool = False, *args: Any, **kwargs: Any):
         super().__init__(*args, **kwargs)
 
         self.layout = QHBoxLayout()
@@ -27,9 +26,11 @@ class QBandFilterWidget(QWidget):
             freq_widget.setValidator(validator)
             self.layout.addWidget(freq_widget)
             self.storage[i] = freq_widget
-            freq_widget.textChanged.connect(self.validate_and_get_values)
+            if debug:
+                freq_widget.textChanged.connect(self.validate_and_get_values)
 
-    def _get_freq_str(self, index: int):
+    @staticmethod
+    def _get_freq_str(index: int):
         return f"f<sub>{index}</sub>"
 
     def validate_and_get_values(self):
@@ -38,24 +39,6 @@ class QBandFilterWidget(QWidget):
             value = self.storage[i].text()
             value = float(value) if value else None
             freqs[i] = value
-
-        print(freqs)
-
-        error_tooltip_kwargs = {"text_position": "right",
-                                "shake_duration": 3000,
-                                "highlight_duration": 3000}
-
-        # for idx1, idx2 in [[1, 2], [3, 4]]:
-        #     widgets = [self.storage[idx1], self.storage[idx2]]
-        #     if (freqs[idx1] is None and freqs[idx2] is not None) or (freqs[idx1] is not None and freqs[idx2] is None):
-        #         text = (f"Parameters {self._get_freq_str(idx1)} and {self._get_freq_str(idx2)} must "
-        #                 f"either be both specified or both empty")
-        #         QErrorToolTip(self, text=text, shake_widgets=widgets, highlight_widgets=widgets, **error_tooltip_kwargs)
-        #         # return
-        #     elif freqs[idx1] > freqs[idx2]:
-        #         text = f"{self._get_freq_str(idx2)} must be greater than or equal to {self._get_freq_str(idx1)}"
-        #         QErrorToolTip(self, text=text, shake_widgets=widgets, highlight_widgets=widgets, **error_tooltip_kwargs)
-        #         # return
 
         problematic_widgets = set()
         recommendation_tips = []
@@ -86,23 +69,20 @@ class QBandFilterWidget(QWidget):
                 recommendations = "<br>".join(recommendation_tips)
             else:
                 recommendations = recommendation_tips[0]
-            QErrorToolTip(self,
-                          text=recommendations,
-                          shake_widgets=list(problematic_widgets),
-                          highlight_widgets=list(problematic_widgets),
-                          **error_tooltip_kwargs)
+
+            text_changed_signals = [w.textChanged for w in problematic_widgets]
+            problematic_widgets = list(problematic_widgets)
+
+            TextToolTip(widget=self, text=recommendations, do_until=text_changed_signals)
+            ShakeToolTip(widgets=list(problematic_widgets), parent=self, do_until=2000)
+            HighlightToolTip(widgets=problematic_widgets, parent=self, do_until=text_changed_signals)
             return None
         else:
             return freqs
 
 
-
-
 if __name__ == "__main__":
     app = QApplication([])
-    window = QBandFilterWidget()
-    # window = QSlider(Qt.Horizontal)
-    # window.valueChanged.connect(lambda x: print(x))
-    # window.rele
+    window = QBandFilterWidget(debug=True)
     window.show()
     app.exec_()
