c61e4dbab84a5accf70f65ba9a2dbfe2190b8ac6
DaloroAT <aleksei.v.tarasov@gmail.com>
2025-11-07 16:43:45 +0100

    minor

--- first_breaks/__init__.py
+++ first_breaks/__init__.py
@@ -22,31 +22,6 @@ def is_macos() -> bool:
     return "darwin" in platform
 
 
-def patch_pathenv_with_default_gpu_paths() -> None:
-    if is_windows():
-        extra_potential_paths = []
-        extra_potential_paths.extend(list(Path(r"C:\Program Files\NVIDIA GPU Computing Toolkit\CUDA").glob("*")))
-        extra_potential_paths.extend(list(Path(r"C:\Program Files\NVIDIA GPU Computing Toolkit").glob("*")))
-        extra_potential_paths.extend(list(Path(r"C:\Program Files\NVIDIA\CUDNN").glob("*")))
-        extra_potential_paths = [str(p) for p in extra_potential_paths]
-        separator = ";"
-    elif is_linux():
-        extra_potential_paths = [
-            "/usr/local/cuda",
-            "/usr/include",
-            "/lib/x86_64-linux-gnu",
-        ]
-        separator = ":"
-    else:
-        extra_potential_paths = []
-        separator = ":"
-
-    os.environ["PATH"] = separator.join([os.environ["PATH"]] + extra_potential_paths)
-
-
-patch_pathenv_with_default_gpu_paths()
-
-
 if is_windows():
     base = os.path.dirname(sys.executable)
     app_pkgs = os.path.join(base, "app_packages")
@@ -66,3 +41,7 @@ if is_windows():
 
     import onnxruntime as ort
 
+
+import onnxruntime
+
+onnxruntime.preload_dlls()
